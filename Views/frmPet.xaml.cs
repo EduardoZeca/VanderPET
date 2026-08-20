ca043b43c6828ca2f1db91ccc20095b72eabba44
EduardoZeca <eduardozeca.tds@gmai.com>
2026-08-20 10:52:46 -0300

    Pickers corrigidos

--- Views/frmPet.xaml.cs
+++ Views/frmPet.xaml.cs
@@ -24,4 +24,30 @@ public partial class frmPet : ContentPage
     {
         await Navigation.PopAsync();
     }
+
+    private void dtPckDataNasc_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DateTime nascimento = e.NewDate.Value;
+        DateTime hoje = DateTime.Today;
+
+        int anos = hoje.Year - nascimento.Year;
+
+        if (nascimento.Date > hoje.AddYears(-anos))
+        {
+            anos--;
+        }
+        if (anos > 0)
+        {
+            txtIdade.Text = anos == 1 ? "1 ano" : $"{anos} anos";
+        }
+        else
+        {
+            int meses = (hoje.Year - nascimento.Year) * 12 + hoje.Month - nascimento.Month;
+            if (hoje.Day < nascimento.Day)
+            {
+                meses--;
+            }
+            txtIdade.Text = meses == 1 ? "1 mês" : $"{meses} meses";
+        }
+    }
 }
